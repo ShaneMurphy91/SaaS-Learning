d2acdb5c72c14c8f085a40a2f145fec86e90a8f3
Shane Murphy <shanemcnamara1991@Gmail.com>
2017-01-19 15:45:39 +0000

    Added contact create action to save to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,8 @@
 class ContactsController < ApplicationController
   def new
-  @contact = Contact.new
+    @contact = Contact.new
   end
+  
 def create
   @contact = Contact.new(contact_params)
   if @contact.save
@@ -10,8 +11,10 @@ def create
      redirect_to new_contact_path, notice: "Error occured."
   end
 end
+
 private
   def contact_params
      params.require(:contact).permit(:name, :email, :comments)
   end
+  
 end
